fcd44579e08efff07f6c276c932d964de88d7f8b
smustfa147 <smustfa147@gmail.com>
2018-11-22 21:40:53 +0000

    test

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#3 SMP PREEMPT Mon Nov 19 23:29:39 UTC 2018"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "kali"
-#define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (GCC) "
--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.10.84-perf-gc36b0bcdc13-dirty"
